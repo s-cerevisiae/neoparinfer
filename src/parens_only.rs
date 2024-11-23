946c72ea4511477633ccf73ea80d2cd491a31b74
spore <spore2050@gmail.com>
2024-11-23 18:13:51 +0800

    refactor: combine the stacks

--- src/parens_only.rs
+++ src/parens_only.rs
@@ -26,42 +26,54 @@ pub enum Side {
     Closing,
 }
 
+struct Context {
+    opening: Paren,
+    first_sibling: Option<Paren>,
+    delta: Delta,
+}
+
 pub fn paren_run(input: &[Line]) -> Result<HashMap<usize, Delta>, Error> {
-    let mut unclosed_parens: Vec<Paren> = Vec::new();
-    let mut first_siblings: HashMap<Level, Paren> = HashMap::new();
     let mut changes = HashMap::new();
-    let mut scope_delta = HashMap::new();
+
+    let mut context: Vec<Context> = Vec::new();
     for (row, line) in input.iter().enumerate() {
-        let level = unclosed_parens.len();
-        let current_delta = scope_delta.get(&level).copied().unwrap_or(0);
-        let indent = (line.indent as Delta + current_delta) as Column;
-        // calculate deltas
-        let min = unclosed_parens.last().map_or(0, |p| p.col);
-        let max = first_siblings
-            .get(&unclosed_parens.len())
+        let cur_delta = context.last().map_or(0, |l| l.delta);
+        let orig_indent = line.indent;
+        let cur_indent = (orig_indent as Delta + cur_delta) as Column;
+        // calculate delta
+        let min = context.last().map_or(0, |l| l.opening.col);
+        let max = context
+            .last()
+            .and_then(|l| l.first_sibling)
             .map_or(Column::MAX, |p| p.col);
-        let delta = indent.clamp(min, max) as Delta - line.indent as Delta;
+        let delta = cur_indent.clamp(min, max) as Delta - orig_indent as Delta;
         if delta != 0 {
             changes.insert(row, delta);
-            scope_delta.insert(level, delta);
         }
 
         // maintain stacks
         for par in &line.parens {
             match par.side {
                 Side::Opening => {
-                    first_siblings.entry(level).or_insert(*par);
-                    unclosed_parens.push(*par);
+                    context
+                        .last_mut()
+                        .map(|l| l.first_sibling.get_or_insert(*par));
+                    context.push(Context {
+                        opening: *par,
+                        first_sibling: None,
+                        delta,
+                    });
                 }
                 Side::Closing => {
-                    first_siblings.remove(&level);
-                    let opening = unclosed_parens.pop().ok_or(Error::Unbalanced)?;
-                    if opening.kind != par.kind {
+                    let cur_layer = context.pop().ok_or(Error::Unbalanced)?;
+                    // todo: pairing usually doesn't work like that
+                    if cur_layer.opening.kind != par.kind {
                         return Err(Error::Unbalanced);
                     }
                 }
             }
         }
     }
+
     Ok(changes)
 }
