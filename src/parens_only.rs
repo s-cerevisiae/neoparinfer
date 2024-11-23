4eb592643b50ded7551211ea0f523a9bd4606771
spore <spore2050@gmail.com>
2024-11-23 15:59:18 +0800

    take changes into consideration

--- src/parens_only.rs
+++ src/parens_only.rs
@@ -30,26 +30,31 @@ pub fn paren_run(input: &[Line]) -> Result<HashMap<usize, Delta>, Error> {
     let mut unclosed_parens: Vec<Paren> = Vec::new();
     let mut first_siblings: HashMap<Level, Paren> = HashMap::new();
     let mut changes = HashMap::new();
+    let mut scope_delta = HashMap::new();
     for (row, line) in input.iter().enumerate() {
+        let level = unclosed_parens.len();
+        let current_delta = scope_delta.get(&level).copied().unwrap_or(0);
+        let indent = (line.indent as Delta + current_delta) as Column;
         // calculate deltas
         let min = unclosed_parens.last().map_or(0, |p| p.col);
         let max = first_siblings
             .get(&unclosed_parens.len())
             .map_or(Column::MAX, |p| p.col);
-        let delta = line.indent.clamp(min, max) as Delta - line.indent as Delta;
+        let delta = indent.clamp(min, max) as Delta - line.indent as Delta;
         if delta != 0 {
             changes.insert(row, delta);
+            scope_delta.insert(level, delta);
         }
 
         // maintain stacks
         for par in &line.parens {
             match par.side {
                 Side::Opening => {
-                    first_siblings.entry(unclosed_parens.len()).or_insert(*par);
+                    first_siblings.entry(level).or_insert(*par);
                     unclosed_parens.push(*par);
                 }
                 Side::Closing => {
-                    first_siblings.remove(&unclosed_parens.len());
+                    first_siblings.remove(&level);
                     let opening = unclosed_parens.pop().ok_or(Error::Unbalanced)?;
                     if opening.kind != par.kind {
                         return Err(Error::Unbalanced);
